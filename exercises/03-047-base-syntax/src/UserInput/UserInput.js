5e35d7ee806a905cd477f8a281e60d5420054793
Franklin Wu <franklin.w.wu@gmail.com>
2018-10-25 04:35:04 -0700

    Fix indentations

--- exercises/03-047-base-syntax/src/UserInput/UserInput.js
+++ exercises/03-047-base-syntax/src/UserInput/UserInput.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const userinput = (props) => {
-    return (
-        <div style={props.style}>
-            <input onChange={props.onChange} value ={props.username} />
-        </div>
-    );
+  return (
+    <div style={props.style}>
+      <input onChange={props.onChange} value ={props.username} />
+    </div>
+  );
 };
 
 export default userinput;
--- exercises/03-047-base-syntax/src/UserOutput/UserOutput.js
+++ exercises/03-047-base-syntax/src/UserOutput/UserOutput.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './UserOutput.css';
 
 const useroutput = () => {
-    return (
-        <div className='UserOutput'>
-            <p>There are more things in heaven and earth, Horatio,</p>
-            <p>Than are dreamt of in your philosophy.</p>
-        </div>
-    );
+  return (
+    <div className='UserOutput'>
+      <p>There are more things in heaven and earth, Horatio,</p>
+      <p>Than are dreamt of in your philosophy.</p>
+    </div>
+  );
 };
 
 export default useroutput;
