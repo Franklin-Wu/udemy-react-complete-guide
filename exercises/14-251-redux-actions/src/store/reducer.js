57c45bd06626c9e132cce2e71723878c8dda4150
Franklin Wu <franklin.w.wu@gmail.com>
2019-01-26 23:26:01 -0800

    Complete 14-251-redux-actions

--- exercises/14-251-redux-actions/src/store/reducer.js
+++ exercises/14-251-redux-actions/src/store/reducer.js
@@ -3,12 +3,26 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+    switch (action.type) {
+        case 'ADD':
+            return {
+                counter: state.counter + action.value
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.value
+            }
+        default:
+            return state;
     }
-    return state;
 };
 
 export default reducer;
